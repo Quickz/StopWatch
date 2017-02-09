dd2255fad93f47368ac258c05f4208c3c522e681
Alex <quickz@inbox.lv>
2017-02-09 23:42:44 +0200

    added the code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # StopWatch
+An online stopwatch.
+<a href="https://quickz.github.io/StopWatch/" target="_blank">Click here</a> to check it out.
---
+++ script.js
@@ -0,0 +1,122 @@
+(function() {
+
+
+	/**
+	 * global variables
+	 *
+	*/
+	var timerElm = document.getElementById("timer");
+	var processing = false;
+	var timer;
+	var seconds = 0;
+	var minutes = 0;
+
+
+	/**
+	 * timer keyboard controls
+	 * 
+	*/
+	document.onkeydown = function(e) {
+
+		switch (e.keyCode)
+		{
+			// space
+			case 32:
+			// enter
+			case 13:
+				e.preventDefault();
+				toggleTimer();
+				break;
+		}
+
+
+	};
+
+
+	/**
+	 * stops/starts the timer
+	 *
+	*/
+	function toggleTimer()
+	{
+		if (processing)
+			stopTimer();
+		else
+			startTimer();
+	}
+
+
+	/**
+	 * stops the timer
+	 *
+	*/
+	function stopTimer()
+	{
+		processing = false;
+		clearTimeout(timer);
+		seconds = 0;
+		minutes = 0;
+	}
+
+
+	/**
+	 * starts the timer
+	 *
+	*/
+	function startTimer()
+	{
+		processing = true;
+		updateDisplay();
+		iterateTimer();
+	}
+
+
+	/**
+	 * adds 10 miliseconds to the timer
+	 *
+	*/
+	function iterateTimer()
+	{
+		timer = setTimeout(function() {
+			
+			seconds += 0.01;
+
+			if (Math.round(seconds) > 59)
+			{
+				minutes++;
+				seconds = 0;
+			}
+
+			updateDisplay();
+			iterateTimer();
+
+		}, 10);
+	}
+
+
+	/**
+	 * updates the timer html element contents
+	 *
+	*/
+	function updateDisplay()
+	{
+		var sec = format(seconds.toFixed(2));
+		var min = format(minutes);
+		timerElm.innerHTML = min + ":" + sec;
+	}
+
+
+	/**
+	 * adds a zero at the beginning
+	 * if the number is less than 10
+	*/
+	function format(value)
+	{
+		if (value < 10)
+			return  "0" + value;
+		else
+			return value;
+	}
+
+
+})();
