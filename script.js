53dea641498e74835e6cc0c0b781afe1359a0269
Alex <quickz@inbox.lv>
2017-02-11 01:41:30 +0200

    implemented scores

--- script.js
+++ script.js
@@ -6,11 +6,15 @@
 	 *
 	*/
 	var timerElm = document.getElementById("timer");
+	var scoreElm = document.getElementById("scores");
+	var scores = [];
 	var processing = false;
 	var timer;
 	var seconds = 0;
 	var minutes = 0;
 	var hours = 0;
+
+	loadScores();
 
 
 	/**
@@ -53,6 +57,8 @@
 	*/
 	function stopTimer()
 	{
+		addNewScore();
+
 		processing = false;
 		clearTimeout(timer);
 		seconds = 0;
@@ -67,10 +73,10 @@
 	*/
 	function startTimer()
 	{
-		
+
 		processing = true;
 		updateDisplay();
-		
+
 		timer = setInterval(function() {
 			
 			seconds += 0.01;
@@ -127,6 +133,67 @@
 			return  "0" + value;
 		else
 			return value;
+	}
+
+
+	/**
+	 * adds and saves the scored time
+	 *
+	*/
+	function addNewScore()
+	{
+		scores.push(timerElm.innerHTML);
+		addScore(timerElm.innerHTML, scores.length - 1);
+		saveScores();
+	}
+
+
+	/**
+	 * adds a score to the html content
+	 *
+	*/
+	function addScore(score, idNumber)
+	{
+		var id = "score" + idNumber;
+		scoreElm.innerHTML += "<div id='" + id  +
+							  "'class='score'>" +
+						  	  score + "</div>";
+
+		var scrElm = document.getElementById(id);// ERROR
+		scrElm.onclick = function() {
+
+			console.log("deleted");
+			scoreElm.removeChild(scrElm);
+			scores.splice(0, 1);
+			saveScores();
+
+		};
+
+	}
+
+
+	/**
+	 * saves the scores into local storage
+	 *
+	*/
+	function saveScores()
+	{
+		localStorage.scores = JSON.stringify(scores);
+	}
+
+
+	/**
+	 * loads the scores from the local storage
+	 *
+	*/
+	function loadScores()
+	{
+		if (!localStorage.scores) return;
+		scores = JSON.parse(localStorage.scores);
+		
+		for (var i = 0; i < scores.length; i++)
+			addScore(scores[i], i);
+			
 	}
 
 
